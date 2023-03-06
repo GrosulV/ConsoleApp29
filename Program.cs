37f12114514af2a96de72bc572ea8c57c625af76
nikitailinskii <anastasiabalickaa435@gmail.com>
2023-03-06 09:34:44 +0200

    reposit

--- Program.cs
+++ Program.cs
@@ -11,12 +11,51 @@ namespace ConsoleApp29
         static void Main(string[] args)
         {
             //Задание 1
-            Random random = new Random();
-            for (int i = 1; i <= 10; i++)
+            //Random random = new Random();
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    int x = random.Next(-40, 40);
+            //    Console.WriteLine(x);
+            //    Console.Read();
+
+            //Задание 2
+            //int count = 0;
+            //Random random = new Random();
+            //for(int i = 1; i <= 15; i++)
+            //{
+            //    double x = -20 + 50 * random.NextDouble();
+            //    Console.WriteLine(x);
+            //    if (x>0)
+            //    { count++;}
+            //    Console.WriteLine($"кол-во полученных чисел={count}");
+            //    Console.Read();
+
             {
-                int x = random.Next(-40, 40);
-                Console.WriteLine(x);
-                Console.Read();
+                //Задание 3
+                int count = 0;
+                Random random = new Random();
+                for (int i = 1; i <= 12; i++)
+                {
+                    double x = -5 + 10 * random.NextDouble();
+                    Console.WriteLine(x);
+                    if(x>0)
+                    Console.Read();
+                }
+                //Задание 4
+                //Random random = new Random();
+                //for (int i = 1; i <= 8; i++)
+                //{
+                //    int x = random.Next(-30, 30);
+                //    Console.WriteLine(x);
+                //    Console.Read();
+
+                //Задание 5
+                //Random random = new Random();
+                //for (int i = 1; i <= 10; i++)
+                //{
+                //    int x = random.Next(-20, 20);
+                //    Console.WriteLine(x);
+                //    Console.Read();
             }
         }
     }
