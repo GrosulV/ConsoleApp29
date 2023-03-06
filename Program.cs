faaf14e2f275916ebc009f02ad26a57e56df25fe
nikitailinskii <anastasiabalickaa435@gmail.com>
2023-03-06 08:58:49 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1
+            Random random = new Random();
+            for (int i = 1; i <= 10; i++)
+            {
+                int x = random.Next(-40, 40);
+                Console.WriteLine(x);
+                Console.Read();
+            }
+        }
+    }
+}
